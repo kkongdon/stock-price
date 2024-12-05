98612dc304fdc00686bb3308c8e44b733a3e8b17
kkongdon <js48016@naver.com>
2024-12-05 20:49:25 +0900

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/stock-price", function (req: any, res: any) {
-  const { serviceKey, numOfRows, pageNo, resultType } =
+  const { serviceKey, numOfRows, pageNo, resultType, likeItmsNm } =
     req.query;
 
   var api_url =
@@ -10,7 +10,7 @@ app.get("/stock-price", function (req: any, res: any) {
   var request = require("request");
   var options = {
     url: api_url,
-    qs: { serviceKey, numOfRows, pageNo, resultType },
+    qs: { serviceKey, numOfRows, pageNo, resultType, likeItmsNm },
   };
 
   request.get(options, function (error: any, response: any, body: any) {
@@ -26,6 +26,6 @@ app.get("/stock-price", function (req: any, res: any) {
 
 app.listen(3000, function () {
   console.log(
-    "http://127.0.0.1:3000/stock-price?serviceKey=u6LTeM%2FaiScn%2FEGgwHFzHgzkg4LFRF6FmjTVuQPcgTKTjJBo%2BVGWSu%2FFMVxPYpZqR0dA2dtLTq5hYZ3kIkB%2BfA%3D%3D&numOfRows=1&pageNo=1 app listening on port 3000!"
+    "http://127.0.0.1:3000/stock-price?serviceKey=u6LTeM%2FaiScn%2FEGgwHFzHgzkg4LFRF6FmjTVuQPcgTKTjJBo%2BVGWSu%2FFMVxPYpZqR0dA2dtLTq5hYZ3kIkB%2BfA%3D%3D&numOfRows=1&pageNo=1&likeItmsNm=삼성전자 app listening on port 3000!"
   );
 });
